e3edfee2f768a22dc0dd91101a3bc535430760f8
realhsb <realhsb@gmail.com>
2025-08-23 05:23:51 +0900

    Create a project

---
+++ FireBug/FireBug/FireBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBugApp.swift
+//  FireBug
+//
+//  Created by Subeen on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FireBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
